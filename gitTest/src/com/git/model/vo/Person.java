a1396133d00edadfd94653bda7e6e3abb7430c96
simjw <simjw@DESKTOP-IMCTETS>
2020-09-15 11:08:51 +0900

    01_person add properties name,age -jw-

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -2,5 +2,30 @@ package com.git.model.vo;
 
 public class Person {
 	//회원정보를 저장하는 클래스
+	private String name;
+	private int age;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
 	
 }
