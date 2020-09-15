9dd8108e66f74a4f2a0ffe745e17a8615833dc8a
master <simjw@DESKTOP-IMCTETS>
2020-09-15 11:21:51 +0900

    05_재수정 person 객체 height 빼고 취미넣음

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -5,7 +5,8 @@ public class Person {
 	private String name;
 	private int age;
 	private String address;
-	private double height;
+	private String[] hobby;
+	private String phone;
 	
 	
 	public Person() {
@@ -36,13 +37,22 @@ public class Person {
 		this.address = address;
 	}
 
-	public double getHeight() {
-		return height;
+	public String[] getHobby() {
+		return hobby;
 	}
 
-	public void setHeight(double height) {
-		this.height = height;
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
 	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
 
 	
 	
