a20f42165abef118939b9a3ef99340ab5a617a1c
master <simjw@DESKTOP-IMCTETS>
2020-09-15 11:16:02 +0900

    04_person에 주소,키 추가 -jw-

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -4,6 +4,9 @@ public class Person {
 	//회원정보를 저장하는 클래스
 	private String name;
 	private int age;
+	private String address;
+	private double height;
+	
 	
 	public Person() {
 		// TODO Auto-generated constructor stub
@@ -24,6 +27,24 @@ public class Person {
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	
 	
 	
 	
